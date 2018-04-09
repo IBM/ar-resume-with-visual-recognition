82ee49251f23b42bc5ed84181cb54926b98e61bf
Sanjeev Ghimire <sanjeev.ghimire@ibm.com>
2018-04-09 13:39:54 -0500

    revert back the idea of Credentials Sample and closes #15

--- ResumeAR/Credentials.swift
+++ ResumeAR/Credentials.swift
@@ -2,11 +2,11 @@
 //  Credentials.swift
 //  ResumeAR
 //
-//  Created by Sanjeev Ghimire on 11/1/17.
-//  Copyright © 2017 Sanjeev Ghimire. All rights reserved.
+//  Created by Sanjeev Ghimire on 4/9/18.
+//  Copyright © 2018 Sanjeev Ghimire. All rights reserved.
 //
 
-public struct CredentialsSample {
+public struct Credentials {
     // Visual Recognition API details
     public static let VR_API_KEY = "<VR API key>"
     public static let VERSION = "2017-11-08"
@@ -14,10 +14,10 @@ public struct CredentialsSample {
     // Cloudant API details
     public static let CLOUDANT_USERNAME = "<username>";
     public static let CLOUDANT_PASSWORD = "<password>";
-    public static let CLOUDANT_HOST = "<host>";
+    public static let CLOUDANT_HOST = "<host>";    
     public static let CLOUDANT_PORT = 443;
-    public static let CLOUDANT_URL = "https://<cloudant_url>"
+    public static let CLOUDANT_URL = "<cloudant url>";
     
     // Cloudant database name
-    public static let CLOUDANT_DATABASE = "<database name>";
+    public static let CLOUDANT_DATABASE = "resumear";
 }
